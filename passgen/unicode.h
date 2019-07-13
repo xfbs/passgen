5854eae8c78b99dfa8a4a88467666562fd33cdc6
Patrick Elsen <pelsen@xfbs.net>
2019-07-13 11:57:44 +0200

    adds some comments and some code for unicode reader

--- passgen/unicode.h
+++ passgen/unicode.h
@@ -1,12 +1,37 @@
+/// @file unicode.h
+///
+/// Methods for decoding unicode characters from a reader.
+
 #pragma once
 #include "passgen/reader.h"
+#include <stdint.h>
 
 struct unicode_reader_t;
 
 struct unicode_reader_t {
+  /// The reader to read from.
   reader_t reader;
+
+  /// The buffer, since we don't know how long each unicode character is.
+  char buffer[4];
+
+  /// How much data is still in the buffer.
+  size_t buffered;
+
+  /// How many unicode characters we've read.
+  size_t amount;
 };
 
 typedef struct unicode_reader_t unicode_reader_t;
 
+/// Creates a new unicode reader from a given reader.
+unicode_reader_t unicode_reader(reader_t reader);
 
+/// Read @p amount unicode characters into @p dest.
+read_result unicode_read(unicode_reader_t *reader, uint32_t *dest, size_t amount);
+
+/// How many unicode characters we've read so far.
+size_t unicode_amount(unicode_reader_t *reader);
+
+/// How many bytes we've consumed so far.
+size_t unicode_size(unicode_reader_t *reader);
--- src/unicode.c
+++ src/unicode.c
@@ -1,2 +1,37 @@
 #include "passgen/unicode.h"
+#include <utf8proc.h>
 
+#define MIN_BUFLEN 8
+
+unicode_reader_t unicode_reader(reader_t reader) {
+  return (unicode_reader_t) {
+    .reader = reader,
+    .buffer = {0},
+    .buffered = 0,
+    .amount = 0,
+  };
+}
+
+read_result unicode_read(unicode_reader_t *reader, uint32_t *dest, size_t amount) {
+  size_t read;
+  for(read = 0; read < amount; ++read) {
+    if(reader->buffered < MIN_BUFLEN) {
+    }
+  }
+
+  read_result res = {
+    .ok = true,
+    .eof = true,
+    .read = read,
+  };
+
+  return res;
+}
+
+size_t unicode_amount(unicode_reader_t *reader) {
+  return reader->amount;
+}
+
+size_t unicode_size(unicode_reader_t *reader) {
+  return reader_pos(&reader->reader) - reader->buffered;
+}
