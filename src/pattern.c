7560c1edba5c26f1845fddb8e9bd4d5937eb2a5f
Patrick Elsen <pelsen@xfbs.net>
2019-08-14 19:19:40 +0200

    works on token parser

--- src/pattern.c
+++ src/pattern.c
@@ -542,11 +542,48 @@ struct pattern_token pattern_token_regular(int32_t codepoint) {
   };
 }
 
+struct pattern_token pattern_token_escaped(int32_t codepoint) {
+  return (struct pattern_token) {
+    .type = PATTERN_TOKEN_ESCAPED,
+    .codepoint = codepoint
+  };
+}
+
 /// Parse the next token, without advancing the unicode reader.
 struct pattern_token pattern_token_peek(const struct unicode_iter *iter) {
   struct unicode_iter iter_copy = *iter;
   return pattern_token_next(&iter_copy);
 }
+
+struct pattern_token pattern_token_unicode(struct unicode_iter *iter) {
+}
+
+struct pattern_token pattern_token_wordlist(struct unicode_iter *iter) {
+}
+
+typedef struct pattern_token pattern_token_special(struct unicode_iter *iter);
+
+struct special_chars {
+  int32_t chr;
+  int32_t dest;
+  pattern_token_special *parser;
+};
+
+static struct special_chars special_chars[] = {
+  {'a', '\a', NULL}, /// alert
+  {'b', '\b', NULL}, /// no idea
+  {'e', '\033', NULL}, /// escape
+  {'f', '\f', NULL}, /// no idea
+  {'n', '\n', NULL}, /// newline
+  {'r', '\r', NULL}, /// carriage return
+  {'t', '\t', NULL}, /// tab
+  {'v', '\v', NULL}, /// vertical tab
+  {'\\', '\\', NULL}, /// backspace
+  {'u', 0, pattern_token_unicode},
+  {'w', 0, pattern_token_wordlist},
+};
+
+static const size_t special_chars_size = sizeof(special_chars);
 
 struct pattern_token pattern_token_next_complex(struct unicode_iter *iter) {
   struct unicode_iter_result result;
@@ -557,7 +594,17 @@ struct pattern_token pattern_token_next_complex(struct unicode_iter *iter) {
     return pattern_token_error(iter->pos);
   }
 
-  return pattern_token_error(0);
+  for(size_t i = 0; i < special_chars_size; ++i) {
+    if(result.codepoint == special_chars[i].chr) {
+      if(special_chars[i].parser) {
+        return special_chars[i].parser(iter);
+      } else {
+        return pattern_token_escaped(special_chars[i].dest);
+      }
+    }
+  }
+
+  return pattern_token_error(iter->pos);
 }
 
 /// Parse the next token, advancing the unicode reader.
