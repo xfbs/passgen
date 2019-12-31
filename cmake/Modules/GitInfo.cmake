044d8d12c32f2a8f41f11a4ecc5376223dcf7cac
Patrick Elsen <pelsen@xfbs.net>
2019-12-31 14:08:56 +0100

    adds git tag commit support for cmake

---
+++ cmake/Modules/GitInfo.cmake
@@ -0,0 +1,18 @@
+find_package(Git QUIET REQUIRED)
+
+macro(GIT_INFO)
+execute_process(
+    COMMAND "${GIT_EXECUTABLE}" rev-parse HEAD
+    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    RESULT_VARIABLE ret
+    OUTPUT_VARIABLE GIT_COMMIT
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+if(NOT "${ret}" STREQUAL "0")
+    message(FATAL_ERROR "Fatal error getting the git commit hash. Set BUILD_GIT_INFO false to disable.")
+endif()
+
+set_property(GLOBAL APPEND
+    PROPERTY CMAKE_CONFIGURE_DEPENDS
+    "${CMAKE_SOURCE_DIR}/.git/index")
+endmacro(GIT_INFO)
