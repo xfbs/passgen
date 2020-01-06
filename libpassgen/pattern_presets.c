fea60734ac5781bcd1c2b2d2fe13d7ddec03f319
Patrick Elsen <pelsen@xfbs.net>
2020-01-06 19:06:54 +0100

    moves passgen and utils into bin

--- libpassgen/pattern_presets.c
+++ libpassgen/pattern_presets.c
@@ -3,6 +3,6 @@
 pattern_preset pattern_presets[] = {
     { "apple1", "[a-zA-Z0-9]{3}(-[a-zA-Z0-9]{3}){3}" },
     { "apple2", "[a-zA-Z0-9]{6}(-[a-zA-Z0-9]{6}){2}" },
-    { "pxkcd", "(\\p{8}-){3}\\p{8}" },
+    { "xkcd", "(\\p[english]{6,8}-){3}\\p[english]{5,8}" },
     { NULL, NULL },
 };
