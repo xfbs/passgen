0ccc283cbf79b8ce716e30673ee27e186501c677
Patrick Elsen <pelsen@xfbs.net>
2020-03-13 13:59:32 +0100

    adds experimental stack-based parser

---
+++ include/passgen/parser.h
@@ -0,0 +1,30 @@
+#include "pattern.h"
+#include "token_new.h"
+
+enum parser_state_type {
+    PARSER_OUTER,
+    PARSER_SEGMENT,
+    PARSER_CHAR,
+    PARSER_DONE,
+};
+
+struct parser_state {
+    enum parser_state_type type;
+    void *data;
+};
+
+struct parser {
+    passgen_array_t state;
+    pattern_t parsed;
+};
+
+// parses a single token.
+int parser_init(
+        struct parser *parser);
+
+int parse_token(
+        struct parser *parser,
+        struct token *token);
+
+int parse_finish(
+        struct parser *parser);
---
+++ libpassgen/parser.c
@@ -0,0 +1,115 @@
+#include "passgen/parser.h"
+
+int parser_init(
+        struct parser *parser) {
+    // initialise parsing stack
+    parser->state = passgen_array_init(sizeof(struct parser_state), NULL);
+
+    // initialise group
+    parser->parsed.group.segments = passgen_array_init(sizeof(pattern_segments_t), NULL);
+
+    // set initial group
+    struct parser_state *state = passgen_array_push(&parser->state, sizeof(struct parser_state), NULL);
+    state->type = PARSER_OUTER;
+    state->data = &parser->parsed.group;
+
+    // set initial segment
+    state = passgen_array_push(&parser->state, sizeof(struct parser_state), NULL);
+    state->type = PARSER_SEGMENT;
+    state->data = passgen_array_push(&parser->parsed.group.segments, sizeof(pattern_segments_t), NULL);
+
+    return 0;
+}
+
+int parse_token_outer(
+        struct parser *parser,
+        struct token *token,
+        struct pattern_group *group);
+
+int parse_token_segment(
+        struct parser *parser,
+        struct token *token,
+        struct pattern_segments *segments);
+
+int parse_token(
+        struct parser *parser,
+        struct token *token) {
+
+    int ret;
+
+    do {
+        // get current state
+        struct parser_state *state = passgen_array_get(
+                &parser->state,
+                sizeof(struct parser_state),
+                parser->state.len - 1);
+
+        switch(state->type) {
+            case PARSER_OUTER:
+                ret = parse_token_outer(parser, token, state->data);
+                break;
+            case PARSER_SEGMENT:
+                ret = parse_token_segment(parser, token, state->data);
+                break;
+        }
+    } while(ret > 0);
+
+    return ret;
+}
+
+int parse_token_outer(
+        struct parser *parser,
+        struct token *token,
+        struct pattern_group *group) {
+    if(token->codepoint == '|') {
+        // create new segment and parser state
+        struct parser_state *state = passgen_array_push(
+                &parser->state,
+                sizeof(struct parser_state),
+                NULL);
+
+        state->type = PARSER_SEGMENT;
+
+        state->data = passgen_array_push(
+                &parser->parsed.group.segments,
+                sizeof(pattern_segments_t),
+                NULL);
+
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+int parse_token_segment(
+        struct parser *parser,
+        struct token *token,
+        struct pattern_segments *segments) {
+
+    // check if this is a delimiter, and in that case, bubble up
+    if(token->codepoint == '|') {
+        passgen_array_pop(&parser->state, sizeof(struct parser_state), NULL);
+        return 1;
+    }
+
+    struct pattern_segment *item = passgen_array_push(&segments->items, sizeof(struct pattern_segment), NULL);
+
+    // we're supposed to read something in.
+    if(token->codepoint == '(') {
+        // start new group
+    }
+
+    if(token->codepoint == '[') {
+        // start new ranges
+    }
+
+    item->kind = PATTERN_CHAR;
+    item->data.character.codepoint = token->codepoint;
+
+    return 0;
+}
+
+int parse_finish(
+        struct parser *parser) {
+    return 0;
+}
