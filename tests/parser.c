8fb7c56b14cfc58c83d1c1bf3ffc5e8533a6b96d
Patrick Elsen <pelsen@xfbs.net>
2020-04-09 15:50:35 +0200

    expands test

--- tests/parser.c
+++ tests/parser.c
@@ -85,7 +85,8 @@ test_result test_parser_multi_groups(void) {
     struct parser parser = {0};
     struct passgen_token_parser token_parser = {0};
     struct passgen_token token = {0};
-    passgen_pattern_segment_t *segment;
+    struct passgen_pattern_segment *segment;
+    struct passgen_pattern_item *item;
 
     assert(0 == parser_init(&parser));
     assert(passgen_token_parse(&token_parser, &token, 'a') == PASSGEN_TOKEN_INIT);
@@ -100,8 +101,18 @@ test_result test_parser_multi_groups(void) {
     segment = passgen_pattern_group_get_segment(&parser.pattern.group, 0);
     assert(1 == segment->items.len);
 
+    item = passgen_pattern_segment_get_item(segment, 0);
+    assert(item);
+    assert(item->kind == PASSGEN_PATTERN_CHAR);
+    assert(item->data.character.codepoint == 'a');
+
     segment = passgen_pattern_group_get_segment(&parser.pattern.group, 1);
     assert(1 == segment->items.len);
+
+    item = passgen_pattern_segment_get_item(segment, 0);
+    assert(item);
+    assert(item->kind == PASSGEN_PATTERN_CHAR);
+    assert(item->data.character.codepoint == 'b');
 
     return test_ok;
 }
