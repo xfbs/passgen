2ad8507825f3dcebdab89378b8a9fae83dfddffe
Patrick Elsen <pelsen@xfbs.net>
2020-04-09 17:21:55 +0200

    adds clangformat target

---
+++ cmake/ClangFormat.cmake
@@ -0,0 +1,45 @@
+# Copyright Tomas Zeman 2019.
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+function(clangformat_setup)
+  set(CLANGFORMAT_EXECUTABLE "clang-format" CACHE STRING "Name of the clang-format executable")
+
+  if(NOT EXISTS ${CLANGFORMAT_EXECUTABLE})
+    find_program(clangformat_executable_tmp ${CLANGFORMAT_EXECUTABLE})
+    if(clangformat_executable_tmp)
+      set(CLANGFORMAT_EXECUTABLE ${clangformat_executable_tmp})
+      unset(clangformat_executable_tmp)
+      message(STATUS "ClangFormat: clang-format found at ${CLANGFORMAT_EXECUTABLE}")
+    else()
+      message(STATUS "ClangFormat: clang-format not found at ${CLANGFORMAT_EXECUTABLE}")
+    endif()
+  endif()
+
+  add_custom_target(clangformat)
+endfunction()
+
+function(clangformat_add target)
+  foreach(clangformat_source ${ARGN})
+    get_filename_component(clangformat_source ${clangformat_source} ABSOLUTE)
+    list(APPEND clangformat_sources ${clangformat_source})
+  endforeach()
+
+  add_custom_target(${target}_clangformat
+    COMMAND
+      ${CLANGFORMAT_EXECUTABLE}
+      -style=file
+      -i
+      ${clangformat_sources}
+    COMMENT
+      "Formating with ${CLANGFORMAT_EXECUTABLE}"
+  )
+
+  add_dependencies(clangformat ${target}_clangformat)
+endfunction()
+
+function(target_clangformat_setup target)
+  get_target_property(target_sources ${target} SOURCES)
+  clangformat_add(${target} ${target_sources})
+endfunction()
---
+++ cmake/IWYU.cmake
@@ -0,0 +1,31 @@
+# Copyright Tomas Zeman 2019.
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+option(ENABLE_IWYU "Enable IncludeWhatYouUse" OFF)
+
+function(iwyu_setup)
+  if(ENABLE_IWYU)
+    if(NOT EXISTS ${IWYU_EXECUTABLE})
+      find_program(iwyu_executable_tmp NAMES include-what-you-use iwyu)
+      if(iwyu_executable_tmp)
+        set(IWYU_EXECUTABLE ${iwyu_executable_tmp} CACHE STRING "IncludeWhatYouUse executable path")
+        unset(iwyu_executable_tmp)
+        message(STATUS "IncludeWhatYouUse: found at ${IWYU_EXECUTABLE}")
+      else()
+          message(FATAL_ERROR "IncludeWhatYouUse: include-what-you-use binary not found, set manually with -DIWYU_EXECUTABLE=/path/to/include-what-you-use")
+      endif()
+    else()
+      message(STATUS "IncludeWhatYouUse: using manually set ${IWYU_EXECUTABLE}")
+    endif()
+  endif()
+endfunction()
+
+function(target_iwyu_setup target)
+  if(ENABLE_IWYU)
+    set_property(TARGET ${target}
+        PROPERTY
+        CXX_INCLUDE_WHAT_YOU_USE ${IWYU_EXECUTABLE})
+  endif()
+endfunction()
