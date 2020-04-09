98eaf20949a4fc627088454742a4ad2b52459306
Patrick Elsen <pelsen@xfbs.net>
2020-04-09 14:26:57 +0200

    adds convenience functions

--- include/passgen/pattern/segment.h
+++ include/passgen/pattern/segment.h
@@ -3,6 +3,10 @@
 #include "substring.h"
 
 struct passgen_pattern_item;
+struct passgen_pattern_set;
+struct passgen_pattern_char;
+struct passgen_pattern_group;
+struct passgen_pattern_special;
 
 typedef struct passgen_pattern_segment {
   struct passgen_pattern_substring pos;
@@ -17,3 +21,7 @@ void passgen_pattern_segment_init(struct passgen_pattern_segment *segment);
 void passgen_pattern_segment_free(struct passgen_pattern_segment *segment);
 
 struct passgen_pattern_item *passgen_pattern_segment_new_item(struct passgen_pattern_segment *segment);
+struct passgen_pattern_char *passgen_pattern_segment_new_char(struct passgen_pattern_segment *segment);
+struct passgen_pattern_set *passgen_pattern_segment_new_set(struct passgen_pattern_segment *segment);
+struct passgen_pattern_group *passgen_pattern_segment_new_group(struct passgen_pattern_segment *segment);
+struct passgen_pattern_special *passgen_pattern_segment_new_special(struct passgen_pattern_segment *segment);
--- libpassgen/pattern/segment.c
+++ libpassgen/pattern/segment.c
@@ -1,5 +1,7 @@
 #include "passgen/pattern/segment.h"
 #include "passgen/pattern/item.h"
+#include "passgen/pattern/set.h"
+#include "passgen/pattern/group.h"
 
 void passgen_pattern_segment_init(struct passgen_pattern_segment *segment) {
   passgen_array_init(
@@ -12,5 +14,35 @@ void passgen_pattern_segment_free(struct passgen_pattern_segment *segment) {
 }
 
 struct passgen_pattern_item *passgen_pattern_segment_new_item(struct passgen_pattern_segment *segment) {
-  return NULL;
+  struct passgen_pattern_item *item = passgen_array_push(
+      &segment->items,
+      sizeof(struct passgen_pattern_item),
+      NULL);
+
+  return item;
 }
+
+struct passgen_pattern_char *passgen_pattern_segment_new_char(struct passgen_pattern_segment *segment) {
+  struct passgen_pattern_item *item = passgen_pattern_segment_new_item(segment);
+  item->kind = PASSGEN_PATTERN_CHAR;
+
+  return &item->data.character;
+}
+
+struct passgen_pattern_set *passgen_pattern_segment_new_set(struct passgen_pattern_segment *segment) {
+  struct passgen_pattern_item *item = passgen_pattern_segment_new_item(segment);
+  item->kind = PASSGEN_PATTERN_SET;
+  passgen_pattern_set_init(&item->data.set);
+
+  return &item->data.set;
+}
+
+struct passgen_pattern_group *passgen_pattern_segment_new_group(struct passgen_pattern_segment *segment) {
+  struct passgen_pattern_item *item = passgen_pattern_segment_new_item(segment);
+  item->kind = PASSGEN_PATTERN_GROUP;
+  passgen_pattern_group_init(&item->data.group);
+
+  return &item->data.group;
+}
+
+struct passgen_pattern_special *passgen_pattern_segment_new_special(struct passgen_pattern_segment *segment);
