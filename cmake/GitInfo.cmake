ddfd918e1d46e6665e4fc298340ccb6b740af264
Patrick Elsen <pelsen@xfbs.net>
2020-04-12 15:47:54 +0200

    store git describe output

--- cmake/GitInfo.cmake
+++ cmake/GitInfo.cmake
@@ -12,6 +12,17 @@ if(NOT "${ret}" STREQUAL "0")
     message(FATAL_ERROR "Fatal error getting the git commit hash. Set BUILD_GIT_INFO false to disable.")
 endif()
 
+execute_process(
+    COMMAND "${GIT_EXECUTABLE}" describe --tags --long
+    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    RESULT_VARIABLE ret
+    OUTPUT_VARIABLE PROJECT_GIT_DESCRIBE
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+if(NOT "${ret}" STREQUAL "0")
+    message(FATAL_ERROR "Fatal error getting the git describe. Set BUILD_GIT_INFO false to disable.")
+endif()
+
 set_property(GLOBAL APPEND
     PROPERTY CMAKE_CONFIGURE_DEPENDS
     "${CMAKE_SOURCE_DIR}/.git/index")
