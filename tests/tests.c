1b02d4dec4252dbee8e76e1366344de3de19ff6c
Patrick Elsen <pelsen@xfbs.net>
2020-04-12 14:54:44 +0200

    fixes tests binary so it can run by prefix

--- tests/tests.c
+++ tests/tests.c
@@ -2,27 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 
 test_result test_ok = {.ok = true};
+
+static inline bool strprefix(const char *prefix, const char *string) {
+  for(size_t offset = 0; prefix[offset]; offset++) {
+    if(prefix[offset] != string[offset]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 int main(int argc, char *argv[]) {
   size_t failures = 0;
   size_t success = 0;
   if(argc > 1) {
     // go through the arguments.
-    for(size_t r = 1; r < argc; ++r) {
+    for(size_t r = 1; r < (size_t) argc; ++r) {
       // go through the tests.
       for(size_t i = 0; tests[i].name; ++i) {
         // find the test with the name that is the current argument.
-        if(0 == strcmp(argv[r], tests[i].name)) {
+        if(strprefix(argv[r], tests[i].name)) {
           // run the testl
           if(run(tests[i])) {
             success += 1;
           } else {
             failures += 1;
           }
-
-          break;
         }
       }
     }
