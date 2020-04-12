ecdda336225643de57f33165df1ea427bf8a0cf5
Patrick Elsen <pelsen@xfbs.net>
2020-04-12 09:52:01 +0200

    fixes compiler warnings

--- tests/array.c
+++ tests/array.c
@@ -23,35 +23,35 @@ test_result test_array_push(void) {
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][0]);
+  assert(data == (array.data[0] + 0));
   assert(array.len == 1);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][256]);
+  assert(data == (array.data[0] + 256));
   assert(array.len == 2);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][512]);
+  assert(data == (array.data[0] + 512));
   assert(array.len == 3);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][768]);
+  assert(data == (array.data[0] + 768));
   assert(array.len == 4);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[1][0]);
+  assert(data == (array.data[1] + 0));
   assert(array.len == 5);
   assert(array.bins == 4);
   memset(data, 0, 256);
@@ -66,7 +66,7 @@ test_result test_array_push(void) {
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[4][0]);
+  assert(data == (array.data[4] + 0));
   assert(array.len == 17);
   assert(array.bins == 8);
   memset(data, 0, 256);
@@ -84,50 +84,50 @@ test_result test_array_get(void) {
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][0]);
+  assert(data == (array.data[0] + 0));
   assert(array.len == 1);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][256]);
+  assert(data == (array.data[0] + 256));
   assert(array.len == 2);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][512]);
+  assert(data == (array.data[0] + 512));
   assert(array.len == 3);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][768]);
+  assert(data == (array.data[0] + 768));
   assert(array.len == 4);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[1][0]);
+  assert(data == (array.data[1] + 0));
   assert(array.len == 5);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   // no bounds checking
-  assert(passgen_array_get(&array, 256, 0) == &array.data[0][0]);
-  assert(passgen_array_get(&array, 256, 1) == &array.data[0][256]);
-  assert(passgen_array_get(&array, 256, 2) == &array.data[0][512]);
-  assert(passgen_array_get(&array, 256, 3) == &array.data[0][768]);
-  assert(passgen_array_get(&array, 256, 4) == &array.data[1][0]);
+  assert(passgen_array_get(&array, 256, 0) == (array.data[0] + 0));
+  assert(passgen_array_get(&array, 256, 1) == (array.data[0] + 256));
+  assert(passgen_array_get(&array, 256, 2) == (array.data[0] + 512));
+  assert(passgen_array_get(&array, 256, 3) == (array.data[0] + 768));
+  assert(passgen_array_get(&array, 256, 4) == (array.data[1] + 0));
 
   // when compiled in debug mode, this fails due to a runtime check.
-  // assert(passgen_array_get(&array, 256, 5) == &array.data[1][256]);
-  // assert(passgen_array_get(&array, 256, 6) == &array.data[1][512]);
-  // assert(passgen_array_get(&array, 256, 7) == &array.data[1][768]);
+  // assert(passgen_array_get(&array, 256, 5) == (array.data[1] + 256));
+  // assert(passgen_array_get(&array, 256, 6) == (array.data[1] + 512));
+  // assert(passgen_array_get(&array, 256, 7) == (array.data[1] + 768));
 
   passgen_array_free(&array, 256, NULL);
 
@@ -142,7 +142,7 @@ test_result test_array_pop(void) {
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][0]);
+  assert(data == (array.data[0] + 0));
   assert(array.len == 1);
   assert(array.bins == 4);
   memset(data, 0, 256);
@@ -155,42 +155,42 @@ test_result test_array_pop(void) {
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][0]);
+  assert(data == (array.data[0] + 0));
   assert(array.len == 1);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][256]);
+  assert(data == (array.data[0] + 256));
   assert(array.len == 2);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][512]);
+  assert(data == (array.data[0] + 512));
   assert(array.len == 3);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[0][768]);
+  assert(data == (array.data[0] + 768));
   assert(array.len == 4);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[1][0]);
+  assert(data == (array.data[1] + 0));
   assert(array.len == 5);
   assert(array.bins == 4);
   memset(data, 0, 256);
 
   data = passgen_array_push(&array, 256, NULL);
   assert(data);
-  assert(data == &array.data[1][256]);
+  assert(data == (array.data[1] + 256));
   assert(array.len == 6);
   assert(array.bins == 4);
   memset(data, 0, 256);
