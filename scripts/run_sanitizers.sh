ac9d75faf6fe4a9cc74954d12ab40eeecd204f49
Patrick Elsen <pelsen@xfbs.net>
2020-04-14 14:58:25 +0200

    adds script to run sanitizers

---
+++ scripts/run_sanitizers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+source_dir="$1"
+
+function run_sanitizer() {
+  sanitizer="$1"
+  tmp_dir=$(mktemp -d)
+
+  CC=clang ctest --build-and-test \
+    "$source_dir" \
+    "$tmp_dir" \
+    --build-generator Ninja \
+    --build-options \
+    -DCMAKE_BUILD_TYPE=Debug -DUSE_SANITIZER=$sanitizer \
+    --test-command ctest --output-on-failure
+
+  rm -rf "$tmp_dir"
+}
+
+run_sanitizer "Address"
+run_sanitizer "Undefined"
+run_sanitizer "Memory"
+run_sanitizer "Leak"
