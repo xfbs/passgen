8ec2b140dc80c541ba83dd54080db1131b0a779e
Patrick Elsen <pelsen@xfbs.net>
2020-04-25 12:46:44 +0200

    renames pronounceable to markov, adds tests

--- libpassgen/pronounceable.c
+++ libpassgen/pronounceable.c
@@ -8,9 +8,9 @@
 #include "passgen/markov_data.h"
 
 const struct passgen_markov3 *markov_pronounceable_all[] = {
-    &passgen_pronounceable_english,
-    &passgen_pronounceable_german,
-    &passgen_pronounceable_latin,
+    &passgen_markov_english,
+    &passgen_markov_german,
+    &passgen_markov_latin,
     NULL};
 
 passgen_pronounceable_map_t passgen_pronounceable_map[] = {
@@ -29,53 +29,6 @@ passgen_pronounceable_lookup(size_t length, const char *name) {
   }
 
   return PASSGEN_PRONOUNCEABLE_LAST;
-}
-
-const struct passgen_markov0 *passgen_markov2_choose(
-    const struct passgen_markov3 *list,
-    passgen_random_t *rand,
-    int32_t p1,
-    int32_t p2) {
-  const struct passgen_markov2 *list2 = passgen_markov3_find(list, p1);
-  if(!list2) {
-    return NULL;
-  }
-
-  const struct passgen_markov1 *list1 = passgen_markov2_find(list2, p2);
-  if(!list1) {
-    return NULL;
-  }
-
-  size_t choices = list1->frequency_sum;
-  size_t nchoice = passgen_random_u64_max(rand, choices);
-
-  return passgen_markov1_find(list1, nchoice);
-}
-
-size_t passgen_markov2_generate(
-    const struct passgen_markov3 *list,
-    passgen_random_t *rand,
-    int32_t *buf,
-    size_t len) {
-  int32_t p1 = 0, p2 = 0;
-  size_t pos = 0;
-
-  do {
-    const struct passgen_markov0 *choice =
-        passgen_markov2_choose(list, rand, p1, p2);
-    assert(choice);
-
-    /* save codepoint */
-    if(choice->codepoint) {
-      buf[pos] = choice->codepoint;
-    }
-    pos += 1;
-
-    p1 = p2;
-    p2 = choice->codepoint;
-  } while(p2 && pos < len);
-
-  return pos - 1;
 }
 
 size_t passgen_pronounceable(
@@ -141,71 +94,4 @@ size_t passgen_pronounceable_len(
   }
 
   return 0;
-}
-
-static int markov1_find(const void *key_p, const void *item_p) {
-  const int32_t *key = key_p;
-  const struct passgen_markov1 *item = item_p;
-
-  if(*key < item->codepoint) {
-    return -1;
-  } else if(*key > item->codepoint) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-const struct passgen_markov1 *
-passgen_markov2_find(const struct passgen_markov2 *list, int32_t codepoint) {
-  const struct passgen_markov1 *result;
-
-  result = bsearch(
-      &codepoint,
-      list->list,
-      list->list_len,
-      sizeof(struct passgen_markov1),
-      markov1_find);
-
-  return result;
-}
-
-static int markov_find(const void *key_p, const void *item_p) {
-  const int32_t *key = key_p;
-  const struct passgen_markov2 *item = item_p;
-
-  if(*key < item->codepoint) {
-    return -1;
-  } else if(*key > item->codepoint) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-const struct passgen_markov2 *
-passgen_markov3_find(const struct passgen_markov3 *list, int32_t codepoint) {
-  const struct passgen_markov2 *result;
-
-  result = bsearch(
-      &codepoint,
-      list->list,
-      list->list_len,
-      sizeof(struct passgen_markov2),
-      markov_find);
-
-  return result;
-}
-
-const struct passgen_markov0 *
-passgen_markov1_find(const struct passgen_markov1 *list, size_t choice) {
-  for(size_t i = 0; i < list->list_len; i++) {
-    if(choice < list->list[i].frequency) {
-      return &list->list[i];
-    }
-
-    choice -= list->list[i].frequency;
-  }
-
-  return NULL;
 }
