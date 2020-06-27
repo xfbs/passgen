4f693de1114c0cffa2b9e38485cc0ec32de75a5f
Patrick Elsen <pelsen@xfbs.net>
2020-06-27 11:10:12 +0200

    updates

--- include/passgen/data/special.h
+++ include/passgen/data/special.h
@@ -8,6 +8,7 @@
 struct passgen_pattern_special {
   enum passgen_pattern_special_kind kind;
   struct passgen_pattern_repeat length;
+  int32_t *parameters;
   /*
   union {
       enum passgen_pronounceable_type pronounceable;
@@ -15,6 +16,16 @@ struct passgen_pattern_special {
   */
 };
 
+/// Initialise a pattern special with the given char.
+///
+/// Char can be 'p' for pronounceable or 'w' for word list.
 void passgen_pattern_special_init_char(
     struct passgen_pattern_special *special,
     uint32_t type);
+
+void passgen_pattern_special_free(
+    struct passgen_pattern_special *special);
+
+void passgen_pattern_special_add_parameter_cp(
+    struct passgen_pattern_special *special,
+    int32_t codepoint);
