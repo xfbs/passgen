15b170fc9add32278df089d5523c6e8e037e0dcc
Patrick Elsen <pelsen@xfbs.net>
2020-11-26 23:10:53 +0100

    removes compilation error

--- bench/all.c
+++ bench/all.c
@@ -2,15 +2,10 @@
 
 #include "bench.h"
 
-bench_declare(pattern_parse);
-bench_declare(dummy_bench);
-
-struct bench_item bench_dummy;
-struct bench_item bench_pattern_parse;
+extern struct bench_item bench_dummy;
+extern struct bench_item bench_pattern_parse;
 
 struct bench_item items[] = {
-    bench_define(pattern_parse),
-    bench_define(dummy_bench),
     {NULL}};
 
 struct bench_run bench_runs[] = {
