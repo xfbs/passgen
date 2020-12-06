d6a11a629277deed5f35ab12451afcada292c9a3
Patrick Elsen <pelsen@xfbs.net>
2020-12-06 22:39:53 +0100

    adds general-purpose array type

--- include/passgen/data/array.h
+++ include/passgen/data/array.h
@@ -7,37 +7,39 @@
 #include <passgen/config.h>
 #include <stddef.h>
 
-// forward declaration, to remove dependency.
-struct passgen_mem;
-typedef struct passgen_mem passgen_mem_t;
-
+// Generic array data type.
 struct passgen_array {
     void **data;
-    size_t len;
-    size_t bins;
-#ifdef PASSGEN_DEBUG
-    passgen_mem_t *mem;
-    size_t size;
-#endif
+    size_t capacity;
+    size_t items;
 };
 
 typedef struct passgen_array passgen_array_t;
 
+// Initialize array. Optionally takes a capacity, if so, memory that is
+// sufficiently large to hold `capacity` items will be allocated.
 void passgen_array_init(
     passgen_array_t *array,
-    size_t size,
-    passgen_mem_t *mem);
-void *
-passgen_array_push(passgen_array_t *array, size_t size, passgen_mem_t *mem);
-void *passgen_array_get(passgen_array_t *array, size_t size, size_t pos);
-void passgen_array_free(
-    passgen_array_t *array,
-    size_t size,
-    passgen_mem_t *mem);
-void passgen_array_pop(passgen_array_t *array, size_t size, passgen_mem_t *mem);
+    size_t capacity);
 
-/* enable this for more optimised lookups. */
-#if 0
-#    define passgen_array_get(array, size, pos) \
-        ((array)->data[pos / (1024 / size)] + (pos % (1024 / size)) * size)
-#endif
+// Push an item to the back of the array.
+void passgen_array_push(
+    passgen_array_t *array,
+    void *pointer);
+
+// Retrieve an item from the array.
+void *passgen_array_get(
+    passgen_array_t *array,
+    size_t pos);
+
+// Frees an array.
+void passgen_array_free(
+    passgen_array_t *array);
+
+// Pops the last item off the array and returns it.
+void *passgen_array_pop(
+    passgen_array_t *array);
+
+void passgen_array_realloc(
+    passgen_array_t *array,
+    size_t capacity);
--- src/libpassgen/data/array.c
+++ src/libpassgen/data/array.c
@@ -3,122 +3,69 @@
 #include <passgen/config.h>
 #include <passgen/memory.h>
 
-#define BINS_INITIAL        4
-#define BINS_MULTIPLIER     2
-#define BIN_SIZE            1024
-#define ITEMS_PER_BIN(size) (BIN_SIZE / size)
+#define SIZE_INITIAL 4
 
-#undef passgen_array_get
+void passgen_array_realloc(
+    passgen_array_t *array,
+    size_t capacity) {
+    array->data = realloc(array->data, capacity * sizeof(void *));
+    array->capacity = capacity;
+}
 
+// Initialize array. Optionally takes a capacity, if so, memory that is
+// sufficiently large to hold `capacity` items will be allocated.
 void passgen_array_init(
     passgen_array_t *array,
-    size_t size,
-    passgen_mem_t *mem) {
-    assert(ITEMS_PER_BIN(size));
+    size_t capacity) {
+    memset(array, 0, sizeof(*array));
 
-    array->data = NULL;
-    array->len = 0;
-    array->bins = 0;
+    if(capacity) {
+        passgen_array_realloc(array, capacity);
+    }
+}
+
+// Push an item to the back of the array.
+void passgen_array_push(
+    passgen_array_t *array,
+    void *pointer) {
+    if(!array->data) {
+        passgen_array_realloc(array, SIZE_INITIAL);
+    }
+
+    if(array->items == array->capacity) {
+        passgen_array_realloc(array, array->capacity * 2);
+    }
+
+    array->data[array->items] = pointer;
+    array->items += 1;
+}
+
+// Retrieve an item from the array.
+void *passgen_array_get(
+    passgen_array_t *array,
+    size_t pos) {
+
 #ifdef PASSGEN_DEBUG
-    array->size = size;
-    array->mem = mem;
-#else
-    (void) size;
-    (void) mem;
+    assert(pos <= array->items);
 #endif
+
+    return array->data[pos];
 }
 
-void *
-passgen_array_push(passgen_array_t *array, size_t size, passgen_mem_t *mem) {
-    assert(array->size == size);
-    assert(array->mem == mem);
-
-    // make sure we have a list of bins
-    if(!array->bins) {
-        array->data = passgen_malloc(mem, BINS_INITIAL * sizeof(void *));
-        if(!array->data) {
-            return NULL;
-        } else {
-            array->bins = BINS_INITIAL;
-        }
-    }
-
-    // make sure the list of bins is large enough.
-    if(array->len == (array->bins * ITEMS_PER_BIN(size))) {
-        size_t new_bins = array->bins * BINS_MULTIPLIER;
-        void **new_data =
-            passgen_realloc(mem, array->data, new_bins * sizeof(void *));
-        if(!new_data) {
-            return NULL;
-        } else {
-            array->data = new_data;
-            array->bins = new_bins;
-        }
-    }
-
-    size_t bin = array->len / ITEMS_PER_BIN(size);
-    size_t offset = array->len % ITEMS_PER_BIN(size);
-
-    // make sure we have a bin allocated for this
-    if(offset == 0) {
-        array->data[bin] = passgen_malloc(mem, ITEMS_PER_BIN(size) * size);
-        if(!array->data[bin]) {
-            return NULL;
-        }
-    }
-
-    array->len += 1;
-    return (array->data[bin] + offset * size);
-}
-
-void *passgen_array_get(passgen_array_t *array, size_t size, size_t pos) {
-    assert(array->size == size);
-    assert(array->len > pos);
-
-    size_t bin = pos / ITEMS_PER_BIN(size);
-    size_t offset = pos % ITEMS_PER_BIN(size);
-    return (array->data[bin] + offset * size);
-}
-
+// Frees an array.
 void passgen_array_free(
-    passgen_array_t *array,
-    size_t size,
-    passgen_mem_t *mem) {
-    assert(array->size == size);
-    assert(array->mem == mem);
-
-    size_t bins = (array->len + ITEMS_PER_BIN(size) - 1) / ITEMS_PER_BIN(size);
-
-    assert(bins <= array->bins);
-
-    for(size_t i = 0; i < bins; i++) {
-        passgen_free(mem, array->data[i]);
-    }
-
-    if(bins) {
-        passgen_free(mem, array->data);
-    }
+    passgen_array_t *array) {
+    free(array->data);
+    memset(array, 0, sizeof(*array));
 }
 
-void passgen_array_pop(
-    passgen_array_t *array,
-    size_t size,
-    passgen_mem_t *mem) {
-    size_t bin = array->len / ITEMS_PER_BIN(size);
-    size_t offset = array->len % ITEMS_PER_BIN(size);
-
-    // if this is the last element in the bin, release it.
-    if(offset == 1) {
-        passgen_free(mem, array->data[bin]);
-        array->data[bin] = NULL;
-
-        // if this was the last bin, free the whole array.
-        if(bin == 0) {
-            passgen_free(mem, array->data);
-            array->data = NULL;
-            array->bins = 0;
-        }
+// Pops the last item off the array and returns it.
+void *passgen_array_pop(
+    passgen_array_t *array) {
+    if(!array->items) {
+        return NULL;
     }
 
-    array->len -= 1;
+    array->items -= 1;
+    return array->data[array->items];
 }
