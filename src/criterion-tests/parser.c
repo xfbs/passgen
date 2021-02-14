fcb45fda11f00b2f07aa2a584beb21ea9619db21
Patrick Elsen <pelsen@xfbs.net>
2021-02-14 18:11:43 +0100

    updates parser tests

--- src/criterion-tests/parser.c
+++ src/criterion-tests/parser.c
@@ -3,10 +3,12 @@
 #include <passgen/data/parser.h>
 #include <passgen/data/pattern_kind.h>
 
-static void parse_codepoint(uint32_t codepoint) {
+// Parse a variable number of codepoints, and make sure that they all show up
+// as chars. This doesn't work with anything that is escaped.
+static void parse_codepoints(size_t length, uint32_t codepoints[]) {
     struct passgen_parser parser;
     passgen_parser_init(&parser);
-    passgen_parser_unicode(&parser, &codepoint, 1);
+    passgen_parser_unicode(&parser, &codepoints[0], length);
     cr_assert(passgen_parse_finish(&parser) == 0);
 
     cr_assert_eq(parser.pattern.group.segments.len, 1);
@@ -16,12 +18,23 @@ static void parse_codepoint(uint32_t codepoint) {
     cr_assert(segment);
     cr_assert_eq(segment->items.len, 1);
 
-    struct passgen_pattern_item *item;
-    item = passgen_pattern_segment_get_item(segment, 0);
+    size_t item_num = 0;
+    size_t char_num = 0;
+    for(size_t pos = 0; pos < length; pos++) {
+        cr_assert(item_num < segment->items.len);
 
-    cr_assert_eq(item->kind, PASSGEN_PATTERN_CHAR);
-    cr_assert_eq(item->data.chars.codepoints[0], codepoint);
-    cr_assert_eq(item->data.chars.count, 1);
+        struct passgen_pattern_item *item;
+        item = passgen_pattern_segment_get_item(segment, item_num);
+        cr_assert_eq(item->kind, PASSGEN_PATTERN_CHAR);
+        cr_assert_eq(item->data.chars.codepoints[char_num], codepoints[pos]);
+
+        // move on to next item or char
+        char_num++;
+        if(char_num == item->data.chars.count) {
+            char_num = 0;
+            item_num++;
+        }
+    }
 
     passgen_parser_free(&parser);
 }
@@ -40,11 +53,14 @@ Test(parser, empty) {
     passgen_parser_free(&parser);
 }
 
-Test(parser, codepoint) {
-    parse_codepoint('a');
-    parse_codepoint('!');
-    parse_codepoint('-');
-    parse_codepoint(',');
-    parse_codepoint(0xFF);
-    parse_codepoint(0x1F642);
+Test(parser, codepoints) {
+    parse_codepoints(1, (uint32_t []){'a'});
+    parse_codepoints(1, (uint32_t []){'!'});
+    parse_codepoints(1, (uint32_t []){'-'});
+    parse_codepoints(1, (uint32_t []){','});
+    parse_codepoints(1, (uint32_t []){0xFF});
+    parse_codepoints(1, (uint32_t []){0x1F642});
+    parse_codepoints(2, (uint32_t[]){'a', 'b'});
+    parse_codepoints(2, (uint32_t[]){'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'});
+    parse_codepoints(2, (uint32_t[]){'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'});
 }
