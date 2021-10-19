546a2b361e6256d8dc87996f94d6e4e5688cccef
Patrick Elsen <pelsen@xfbs.net>
2021-10-19 15:09:09 +0200

    Renames passgen_version_t -> passgen_version

--- include/passgen/version.h
+++ include/passgen/version.h
@@ -2,20 +2,20 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef struct passgen_version_t {
+typedef struct passgen_version {
     uint8_t maj;
     uint8_t min;
     uint8_t rev;
-} passgen_version_t;
+} passgen_version;
 
 /// Returns the version of passgen.
-passgen_version_t passgen_version();
+passgen_version passgen_version_get();
 
 /// Returns a pointer to a string representing the current version.
 const char *passgen_version_str();
 
 /// Test if the version of passgen is at least as big as the one passed.
-bool passgen_version_min(passgen_version_t version);
+bool passgen_version_min(passgen_version version);
 
 /// Checks if this is a debug build.
 bool passgen_is_debug(void);
