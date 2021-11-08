bdc07fd218e76a6defacec72e2f54ccf740b8a37
Patrick Elsen <pelsen@xfbs.net>
2021-11-08 02:25:29 +0100

    Adds toolchain file for macOS, simplify CI config

---
+++ scripts/macos-toolchain.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(CMAKE_C_COMPILER /opt/osxcross/bin/x86_64h-apple-darwin18-clang)
+set(CMAKE_CXX_COMPILER /opt/osxcross/bin/x86_64h-apple-darwin18-clang++)
+set(CMAKE_AR /opt/osxcross/bin/x86_64-apple-darwin18-ar)
+set(CMAKE_RANLIB /opt/osxcross/bin/x86_64h-apple-darwin18-ranlib)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
