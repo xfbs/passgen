8bc8352483401116da7aad9e218d7d183eeb0efd
Patrick Elsen <pelsen@xfbs.net>
2022-07-24 20:32:34 -0500

    Makes range codepoints unsigned

--- include/passgen/data/range.h
+++ include/passgen/data/range.h
@@ -13,10 +13,14 @@
 
 /// Represents a single range of characters, such as `a-z`.
 ///
-///The range has a start and an end character, both are inclusive.
+/// The range has a start and an end character, both are inclusive. The end character
+/// must be equal to or larger than the start character. If they are the same, the range
+/// matches only the single character.
 typedef struct passgen_pattern_range {
-    int32_t start;
-    int32_t end;
+    /// Starting character in the range
+    uint32_t start;
+    /// Final character in the end.
+    uint32_t end;
 
     size_t multiplier;
 } passgen_pattern_range_t;
