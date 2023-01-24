57d3240487daa7d7cd2dae2357e692b5dd2a499c
Patrick Elsen <pelsen@xfbs.net>
2023-01-24 13:25:32 -0500

    Test passgen output in ci-test-passgen script

--- scripts/ci-test-passgen.sh
+++ scripts/ci-test-passgen.sh
@@ -4,24 +4,33 @@ set -euxo pipefail
 # which binary to test
 passgen=$@
 
+function matches() {
+    grep -E "^$1$"
+}
+
+function pattern_matches() {
+    $passgen "$1" | matches "$1"
+}
+
 # test simple grammar
-$passgen "a"
-$passgen "a{2}"
-$passgen "a{1,2}"
-$passgen "abc"
-$passgen "abc|def"
-$passgen "(abc)"
-$passgen "(abc){2}"
-$passgen "(abc){1,2}"
-$passgen "(abc|def)"
-$passgen "(abc|def){2}"
-$passgen "(abc|def){1,2}"
-$passgen "[a]"
-$passgen "[ab]"
-$passgen "[a-z]"
-$passgen "[a-zA-Z0-9#]"
-$passgen "[a-zA-Z0-9#]{2}"
-$passgen "[a-zA-Z0-9#]{1,2}"
+pattern_matches "a"
+pattern_matches "a?"
+pattern_matches "a{2}"
+pattern_matches "a{1,2}"
+pattern_matches "abc"
+pattern_matches "abc|def"
+pattern_matches "(abc)"
+pattern_matches "(abc){2}"
+pattern_matches "(abc){1,2}"
+pattern_matches "(abc|def)"
+pattern_matches "(abc|def){2}"
+pattern_matches "(abc|def){1,2}"
+pattern_matches "[a]"
+pattern_matches "[ab]"
+pattern_matches "[a-z]"
+pattern_matches "[a-zA-Z0-9#]"
+pattern_matches "[a-zA-Z0-9#]{2}"
+pattern_matches "[a-zA-Z0-9#]{1,2}"
 
 # test presets
 $passgen -p apple1
