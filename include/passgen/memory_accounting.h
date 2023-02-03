3c32e0409aaf18dce58b3b6b004acd4649e1d63d
Patrick Elsen <pelsen@xfbs.net>
2023-02-03 07:30:48 -0500

    Removes unused memory headers

--- include/passgen/memory_accounting.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct passgen_mem_accounting_list;
-struct passgen_mem_accounting {
-  struct passgen_mem *mem;
-  struct passgen_mem_accounting_list *list;
-};
-
-typedef struct passgen_mem_accounting passgen_mem_accounting_t;
