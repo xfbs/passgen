31aaa235eeac58200046791de089d6a4955b19a3
Patrick Elsen <pelsen@xfbs.net>
2023-02-03 20:45:00 -0500

    Moves mkdocs config out to repository root
    This change makes is easier to use mkdocs locally (simply by running
    mkdocs serve). It also does not significantly clutter the root of the
    repository.

--- scripts/ci-publish-website.sh
+++ scripts/ci-publish-website.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mv docs/mkdocs.yml .
 mkdir docs/images
 curl -L 'https://unsplash.com/photos/kgRBoAKq-4E/download?force=true&w=2400' --output docs/images/randomness.jpg
 curl -L 'https://unsplash.com/photos/XRNSn4gt8Hs/download?force=true&w=2400' --output docs/images/chaos.jpg
