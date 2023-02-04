6fc04455938fe72a6e93a0c5d3a20b19a0d2bd10
Patrick Elsen <pelsen@xfbs.net>
2023-02-04 14:34:23 -0500

    Adds documentation to debug assertions

--- include/passgen/assert.h
+++ include/passgen/assert.h
@@ -6,16 +6,20 @@
 #include <stddef.h>
 
 #ifdef PASSGEN_DEBUG
+/// Assertion, enabled in debug build.
 #define passgen_assert(cond) \
     if(!(cond)) passgen_assert_fail(#cond, __FILE__, __func__, __LINE__)
 #else
-#define passgen_assert(cond)
+/// Assertion, disabled in release build.
+#define passgen_assert(cond) (void) (cond)
 #endif
 
+/// Prints assertion failure and optionally backtrace.
 void passgen_assert_fail(
     const char *cond,
     const char *file,
     const char *func,
     size_t line);
 
+/// Static assertion.
 #define passgen_static_assert(x) _Static_assert(x)
