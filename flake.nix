4809acc50c683b26d4e919abf3e4b6aab389d05d
Patrick Elsen <pelsen@xfbs.net>
2024-04-01 13:03:20 -0400

    Adds nix flake for passgen.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Random sequence generator based on regular expressions.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        passgen = pkgs.stdenv.mkDerivation {
+            pname = "passgen";
+            version = "1.0";
+            src = ./.;
+            nativeBuildInputs = [ pkgs.cmake pkgs.git ];
+            cmakeFlags = [ "-DBUILD_GIT_INFO=OFF" ];
+        };
+      in
+      {
+        packages.wpa_passphrase = passgen;
+        defaultPackage = passgen;
+        devShell = with pkgs; mkShell {
+          buildInputs = [ passgen ];
+        };
+      });
+}
