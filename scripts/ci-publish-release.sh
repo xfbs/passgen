ed4da449e0be460e10160a11e8d922783424a256
Patrick Elsen <pelsen@xfbs.net>
2024-05-31 20:11:15 +0200

    WIP

--- scripts/ci-publish-release.sh
+++ scripts/ci-publish-release.sh
@@ -1,43 +1,33 @@
 #!/bin/bash
 set -euxo pipefail
 
-mkdir -p public/nightly
-
-cp build-alpine-release/passgen-*-Linux.tar.xz public/passgen-linux-amd64-musl.tar.xz
-cp build-alpine-release/src/cli/passgen public/passgen-linux-amd64-musl.bin
-
-cp build-amd64/passgen-*-Linux.tar.xz public/passgen-linux-amd64.tar.xz
-cp build-amd64/passgen_*_amd64.deb public/passgen-linux-amd64.deb
-
-cp build-aarch64/passgen-*-Linux.tar.xz public/passgen-linux-aarch64.tar.xz
-cp build-aarch64/passgen_*_arm64.deb public/passgen-linux-arm64.deb
-
-cp build-riscv64/passgen-*-Linux.tar.xz public/passgen-linux-riscv64.tar.xz
-cp build-riscv64/passgen_*_riscv64.deb public/passgen-linux-riscv64.deb
-
-cp build-macos/passgen-*-Darwin.tar.xz public/passgen-macos-amd64.tar.xz
-
-cp build-mingw64/passgen-*-win64.zip public/passgen-win64-amd64.zip
-
-cp build-wasi/src/cli/passgen public/passgen-wasi.wasm
-
 chmod 0600 $PASSGEN_SIGNING_KEY
 
 function sign() {
     ssh-keygen -Y sign -f $PASSGEN_SIGNING_KEY -n file $1
 }
 
-sign public/passgen-linux-amd64-musl.tar.xz
-sign public/passgen-linux-amd64-musl.bin
-sign public/passgen-linux-amd64.tar.xz
-sign public/passgen-linux-amd64.deb
-sign public/passgen-linux-aarch64.tar.xz
-sign public/passgen-linux-arm64.deb
-sign public/passgen-linux-riscv64.tar.xz
-sign public/passgen-linux-riscv64.deb
-sign public/passgen-macos-amd64.tar.xz
-sign public/passgen-win64-amd64.zip
-sign public/passgen-wasi.wasm
+function upload() {
+    curl --header "JOB-TOKEN: ${CI_JOB_TOKEN}" --upload-file "$1" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/passgen-${CI_COMMIT_TAG}/$2"
+}
 
-# export keys file
-cp data/passgen.pub public/passgen.pub
+function release() {
+    sign "$1"
+    upload "$1" "$2"
+    upload "$1.sig" "$2.sig"
+}
+
+# release binaries
+release build-alpine-release/passgen-*-Linux.tar.xz public/passgen-linux-amd64-musl.tar.xz
+release build-alpine-release/src/cli/passgen public/passgen-linux-amd64-musl.bin
+release build-amd64/passgen-*-Linux.tar.xz public/passgen-linux-amd64.tar.xz
+release build-amd64/passgen_*_amd64.deb public/passgen-linux-amd64.deb
+release build-aarch64/passgen-*-Linux.tar.xz public/passgen-linux-aarch64.tar.xz
+release build-aarch64/passgen_*_arm64.deb public/passgen-linux-arm64.deb
+release build-riscv64/passgen-*-Linux.tar.xz public/passgen-linux-riscv64.tar.xz
+release build-riscv64/passgen_*_riscv64.deb public/passgen-linux-riscv64.deb
+release build-macos/passgen-*-Darwin.tar.xz public/passgen-macos-amd64.tar.xz
+release build-mingw64/passgen-*-win64.zip public/passgen-win64-amd64.zip
+release build-wasi/src/cli/passgen public/passgen-wasi.wasm
+
+upload data/passgen.pub passgen.pub
